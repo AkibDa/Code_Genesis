03d1c55c32880cdea7fb789d84d5a7cf5ccb2709
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-10-25 21:06:32 +0530

    statring the graph

--- agent/graph.py
+++ agent/graph.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from prompt import *
 from states import *
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 
 load_dotenv()
 
@@ -9,8 +11,14 @@ llm = ChatGroq(model="openai/gpt-oss-120b")
 
 user_prompt = "Create a simple calculator web application"
 
-prompt = planner_prompt(user_prompt)
+def planner_agent(state: dict) -> dict:
+  users_prompt = planner_prompt(state["user_prompt"])
+  resp = llm.with_structured_output(Plan).invoke(users_prompt)
+  return {"plan": resp, END: True}
 
-response = llm.with_structured_output(Plan).invoke(prompt)
+response = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
 
 print(response)
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
