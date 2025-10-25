7cf3715f6f9b47856ce21298bf7341b7bf487e6b
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-10-25 21:15:42 +0530

    adding states for the architect agent

--- agent/states.py
+++ agent/states.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 class File(BaseModel):
   path : str = Field(description="The file path where the file should be created, e.g. 'src/App.js'")
@@ -10,3 +10,11 @@ class Plan(BaseModel):
   techstack : str = Field(description="The tech stack to be used for the app, e.g. 'React', 'Python', 'Javascript', 'Flask', etc.")
   features : list[str] = Field(description="A list of features to be implemented in the app, e.g. 'User authentication', 'Data visualization', etc.")
   files : list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
+  
+class ImplementationTask(BaseModel):
+  filepath: str = Field(description="The path to the file to be modified")
+  task_description: str = Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+
+class TaskPlan(BaseModel):
+  implementation_steps: list[ImplementationTask] = Field(description="A list of steps to be taken to implement the task")
+  model_config = ConfigDict(extra="allow")
